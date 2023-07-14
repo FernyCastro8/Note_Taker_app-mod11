8f1b30770fb4b683494aefa508476ac3ad745fb3
Ferny <fernc28@outlook.com>
2023-07-13 21:00:49 -0400

    fin

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,36 +1,37 @@
 const router = require('express').Router();
 const fs = require('fs');
 const db = require('../db/db.json');
+// Helper for generating unique id
 const uuid = require('uuid');
-// Helper for generating id
 
 
-    // "GET" resquest
+// "GET" resquest
 router.get('/notes', (req, res) => {
     const savedNOtes = db;
     res.json(savedNOtes)
 })
 
 
-    // "POST" request
+// "POST" request
 router.post('/notes', (req, res) => {
     const notes = db;
     const addNotes = req.body;
     console.log(req.body)
     addNotes.id = uuid.v4()
-        // uuid helper
-        notes.push(addNotes);
-        console.log(notes)
-        fs.writeFileSync('./db/db.json', JSON.stringify(notes));
-        res.json(notes);
+    // uuid helper
+    notes.push(addNotes);
+    console.log(notes)
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+    res.json(notes);
 });
 
-router.delete('/notes:id', (req, res) => {
-    const notes = JSON.parse(fs.readFile('./db/db.jdon'));
-    const deleteNote = note.filter((removeNote) => removeNote.readFile !== req.params.id);
-    fs.writeFile('./db/db.json', JSON.stringify(deleteNote));
+router.delete('/notes/:id', (req, res) => {
+    const notes = JSON.parse(fs.readFileSync('./db/db.json'));
+    const deleteNote = notes.filter((removeNote) => removeNote.id !== req.params.id);
+    fs.writeFileSync('./db/db.json', JSON.stringify(deleteNote));
 
     res.json(deleteNote);
 });
 
-module.exports = router
+module.exports = router;
+
